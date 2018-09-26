fe50fc33ea65d6d52490dae9dff86d6bf9dd4f0b
LITTLE-BERTHA <robertjewell808@gmail.com>
2018-09-26 19:05:46 -0400

    Output results in a txt file instead

--- Facebook Messenger Stats/main.py
+++ Facebook Messenger Stats/main.py
@@ -1,8 +1,5 @@
 import json
 import time
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
 
 with open('message.json') as file:
     data = json.load(file)
@@ -13,36 +10,22 @@ months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 count = 1
 
-for x in range(1,7):
+for x in range(2,9+1):
     for y in range(1, months[x - 1] + 1):
-        dates[str(x).zfill(2)+"-"+str(y).zfill(2)] = 0
+        dates[str(x).zfill(2) + "-" + str(y).zfill(2)] = 0
+
+print(len(dates))
 
 for message in data['messages']:
-    date = time.strftime('%m-%d', time.localtime(message['timestamp']))
+    date = time.strftime('%m-%d', time.localtime(message['timestamp_ms'] / 1000))
 
     if date not in dates:
         dates[date] = 1
     else:
         dates[date] += 1
 
-    if "02-" in date and 'content' in message:
-        print(date + ": " + message['content'])
 
-def label(x, pos):
-    if x % 11 == 0 or x == len(dates) - 1:
-        return tuple(dates.keys())[pos]
-
-    return ""
-
-formatter = FuncFormatter(label)
-
-x_range = np.arange(len(dates))
-
-fig, ax = plt.subplots()
-
-plt.bar(x_range, dates.values())
-plt.xticks(x_range, tuple(dates.keys()))
-
-ax.get_xaxis().set_major_formatter(formatter)
-
-plt.show()
+f = open("file.txt", "w")
+for x in dates.values():
+    f.write(str(x) + "\n")
+f.close()
